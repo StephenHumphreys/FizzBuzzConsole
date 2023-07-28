fdd1b98404f15d136d1b1d50c2293b90e93a1175
stephen_humphreys <stephen.humphreys@100fold.org>
2023-07-28 21:13:24 +0100

    Added null check to reflection call to remove compiler warning

--- FizzBuzzConsole/Program.cs
+++ FizzBuzzConsole/Program.cs
@@ -8,6 +8,9 @@
 // TODO: create the instance of FizzBuzz using reflection
 
 Type fizzBuzzType = typeof(FizzBuzz.FizzBuzz);
-MethodInfo method = fizzBuzzType.GetMethod("PlayFizzBuzz");
-FizzBuzz.FizzBuzz fizzBuzz = new();
-Console.WriteLine(method.Invoke(fizzBuzz, new[] { Enumerable.Range(1, 100).ToArray() }));
+MethodInfo? method = fizzBuzzType.GetMethod("PlayFizzBuzz");
+if (method != null)
+{
+    FizzBuzz.FizzBuzz fizzBuzz = new();
+    Console.WriteLine(method.Invoke(fizzBuzz, new[] { Enumerable.Range(1, 100).ToArray() }));
+}
