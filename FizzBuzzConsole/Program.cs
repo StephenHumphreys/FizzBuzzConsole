196c3d67d935629ce133c3247dae91298b7dcc32
stephen_humphreys <stephen.humphreys@100fold.org>
2023-07-28 21:07:54 +0100

    Added a comment explaining how the reflection code works

--- FizzBuzzConsole/Program.cs
+++ FizzBuzzConsole/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System.Reflection;
 
+// Gets the type handle of the FizzBuzz class
+// Then gets the method info of the PlayFizzBuzz method using that type handle
+// Creates an instance of FizzBuzz
+// Then uses the mthod info to invoke the method using the FizzBuzz object created earlier
+// The paramters of method.Invoke are the object and an object[] of all the paramters to the method
+// TODO: create the instance of FizzBuzz using reflection
 
 Type fizzBuzzType = typeof(FizzBuzz.FizzBuzz);
 MethodInfo method = fizzBuzzType.GetMethod("PlayFizzBuzz");
